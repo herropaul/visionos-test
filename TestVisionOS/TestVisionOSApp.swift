2aee8054ee78a8b8b2e5b72176a3b499f3d551d8
Paul Nguon <herropaul@gmail.com>
2023-06-22 21:00:16 -0700

    Initial Commit

---
+++ TestVisionOS/TestVisionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestVisionOSApp.swift
+//  TestVisionOS
+//
+//  Created by Paul Nguon on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct TestVisionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
